5d113e8cb59c7c3654b22c94c33146a1813a0385
anti-duhring <mateus.limavn@gmail.com>
2023-01-28 12:22:16 -0300

    first commit

---
+++ src/main/java/com/passhelm/passhelm/PasshelmApplication.java
@@ -0,0 +1,13 @@
+package com.passhelm.passhelm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasshelmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasshelmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/passhelm/passhelm/PasshelmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passhelm.passhelm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasshelmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
