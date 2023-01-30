fc777aa73cea0521267789be37ed76ef5562d4fa
anti-duhring <mateus.limavn@gmail.com>
2023-01-30 11:02:22 -0300

    feat: User repository

--- src/main/java/com/passhelm/passhelm/service/UserService.java
+++ src/main/java/com/passhelm/passhelm/service/UserService.java
@@ -1,6 +1,8 @@
 package com.passhelm.passhelm.service;
 
 import com.passhelm.passhelm.models.User;
+import com.passhelm.passhelm.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,15 +10,15 @@ import java.util.List;
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public List<User> getAllUsers() {
 
-        return List.of(
-                new User(
-                        "mateusvnlima",
-                        "Mateus Vinicius",
-                        "mateusvnlima@gmail.com",
-                        "123456"
-                )
-        );
+        return userRepository.findAll();
     }
 }
